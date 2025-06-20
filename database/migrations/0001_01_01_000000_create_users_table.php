9f132ce4caa6327583ae7367cfa8d99370f0824c
_RAINHARDER <raihanys03@gmail.com>
2025-06-21 00:53:17 +0700

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'librarian']);
             $table->rememberToken();
             $table->timestamps();
+
+            // Ensure InnoDB engine.
+            $table->engine = 'InnoDB';
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
