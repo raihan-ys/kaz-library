4822af12a5b7c7ce9d779ca6ba86b1de5e09e33d
_RAINHARDER <raihanys03@gmail.com>
2025-06-22 15:12:49 +0700

    Update 2024_11_12_153721_create_member_type_table.php

--- database/migrations/2024_11_12_153721_create_member_type_table.php
+++ database/migrations/2024_11_12_153721_create_member_type_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('member_type', function (Blueprint $table) {
+        Schema::create('member_types', function (Blueprint $table) {
             $table->id();
             $table->string('name');
 
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('member_type');
+        Schema::dropIfExists('member_types');
     }
 };
