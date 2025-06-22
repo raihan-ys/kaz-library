3e6783a9353f24fab0ce8f8e5c3529a93c4b0c7d
_RAINHARDER <raihanys03@gmail.com>
2025-06-22 16:27:17 +0700

    update

--- database/migrations/2024_07_31_022952_create_books_table.php
+++ database/migrations/2024_07_31_022952_create_books_table.php
@@ -18,9 +18,6 @@ class CreateBooksTable extends Migration
             $table->bigInteger('isbn');
             $table->integer('published_year');
 
-            // Adding 'deleted_at' column for soft deletes.
-            $table->softDeletes();
-
             // FK to category.
             $table->unsignedBigInteger('category_id');
 
@@ -48,9 +45,5 @@ class CreateBooksTable extends Migration
     public function down(): void
     {
         Schema::dropIfExists('books');
-        // Dropping 'deleted_at' column for soft deletes.
-        Schema::table('books', function (Blueprint $table) {
-            $table->dropSoftDeletes();
-        });
     }
 };
